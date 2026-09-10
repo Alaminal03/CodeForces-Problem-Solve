8a2e35d21e89b7ddeb276e082c48e374c3ce94bc
Al Amin <111590949+Alaminal03@users.noreply.github.com>
2026-09-10 16:37:47 +0600

    Add files via upload

---
+++ A_Creating_Abbreviations.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int man()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int m, n;
+        cin >> m >> n;
+
+    
+        for (int i = 0; i < m;i++)
+        {
+            string s;
+            cin >> s[i];
+        }
+        for (int i = 0; i < n;i++)
+        {
+            string p;
+            cin >> p[i];
+        }
+
+        for (int i = 0; i < n;i++)
+        {
+
+        }
+    }
+
+    return 0;
+}
