3c25a1c51a78031232c9baaf0e8406dd9a754a97
Al Amin <111590949+Alaminal03@users.noreply.github.com>
2026-09-21 00:25:36 +0600

    Add files via upload

---
+++ Second_Half_Sum.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    int a[n];
+
+    for (int i = 0; i < n;i++)
+    {
+        cin >> a[i];
+    }
+    int sum = 0;
+    for (int i = n / 2; i < n;i++)
+    {
+        sum = sum + a[i];
+    }
+    cout << sum << endl;
+
+    return 0;
+}
